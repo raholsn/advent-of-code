0953198990e32d2c021cf9d1388c754f0dddcf7c
Rasmus olsson <rasmus_olsson_@hotmail.com>
2020-12-05 21:33:35 +0100

    Day 4

---
+++ day4/index.ts
@@ -0,0 +1,102 @@
+const day4 = async () => {
+  const part1 = async () => {
+    const isValid = (row: string, word: string) => {
+      if (row.includes(word)) return true;
+
+      return false;
+    };
+
+    const validPassports = (await Deno.readTextFile("./input.txt"))
+      .split("\n\n")
+      .filter((pp) => {
+        return (
+          isValid(pp, "byr") &&
+          isValid(pp, "iyr") &&
+          isValid(pp, "eyr") &&
+          isValid(pp, "hgt") &&
+          isValid(pp, "hcl") &&
+          isValid(pp, "ecl") &&
+          isValid(pp, "pid")
+        );
+      });
+
+    console.log(validPassports.length);
+  };
+
+  const part2 = async () => {
+    interface Dictionary<T> {
+      [Key: string]: T;
+    }
+
+    type Fields = Dictionary<string>;
+
+    const mapToPassportFields = (passport: string[]): Fields => {
+      const fields: Fields = {};
+      passport.forEach((f) => {
+        const key = f.split(":")[0];
+        const value = f.split(":")[1];
+        fields[key] = value;
+      });
+
+      return fields;
+    };
+
+    const hasRequiredPassportFields = (fields: Fields) => {
+      return ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].every(
+        (field) => field in fields
+      );
+    };
+
+    const hasValidBirthDate = (value: number) => value >= 1920 && value <= 2002;
+
+    const hasValidIssueYear = (value: number) => value >= 2010 && value <= 2020;
+
+    const hasValidExpirationYear = (value: number) =>
+      value >= 2020 && value <= 2030;
+
+    const hasValidHeight = (value: string) => {
+      if (value.endsWith("cm")) {
+        value = value.replace("cm", "");
+        return +value >= 150 && +value <= 193;
+      }
+      if (value.endsWith("in")) {
+        value = value.replace("in", "");
+        return +value >= 59 && +value <= 76;
+      }
+      return false;
+    };
+
+    const hasValidHairColor = (value: string) =>
+      RegExp(/#[0-9a-f]{6}/).test(value);
+
+    const hasValidEyeColor = (value: string) =>
+      ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(value);
+
+    const hasValidPassportId = (value: string) => value.length === 9;
+
+    const validPassports = (await Deno.readTextFile("./input.txt"))
+      .split("\n\n")
+      .filter((passport) => {
+        const passportDetails = passport.split(/ |\n/);
+        const passportFields = mapToPassportFields(passportDetails);
+
+        return (
+          hasRequiredPassportFields(passportFields) &&
+          hasValidBirthDate(+passportFields["byr"]) &&
+          hasValidIssueYear(+passportFields["iyr"]) &&
+          hasValidExpirationYear(+passportFields["eyr"]) &&
+          hasValidHeight(passportFields["hgt"]) &&
+          hasValidHairColor(passportFields["hcl"]) &&
+          hasValidEyeColor(passportFields["ecl"]) &&
+          hasValidPassportId(passportFields["pid"])
+        );
+      });
+
+    console.log(validPassports.length);
+  };
+
+  await part1();
+  await part2();
+};
+
+await day4();
