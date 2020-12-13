b6ef55488b104b355933444af34ad50abd3464cd
Rasmus olsson <rasmus_olsson_@hotmail.com>
2020-12-13 10:18:46 +0100

    Day 10 p2

--- day10/index.ts
+++ day10/index.ts
@@ -4,8 +4,10 @@ const day10 = async () => {
     .map((x) => +x)
     .sort((a, b) => a - b);
 
+  input.unshift(0);
+
   const part1 = () => {
-    let nrOf1s = 1;
+    let nrOf1s = 0;
     let nrOf3s = 1;
     for (let i = 0; i < input.length; i++) {
       const joltage = input[i];
@@ -21,8 +23,24 @@ const day10 = async () => {
     console.log(nrOf1s * nrOf3s);
   };
 
-  const part2 = () => {};
+  const part2 = () => {
+    const count: number[] = [1];
+    for (let i = 0; i < input.length; i++) {
+      const range = input.slice(0, i);
+      for (let j = 0; j < range.length; j++) {
+        const difference = input[i] - input[j];
+        if (difference <= 3) {
+          if (count[i] === undefined) count[i] = 0;
+          if (count[j] === undefined) count[j] = 0;
+          count[i] += count[j];
+        }
+      }
+    }
+
+    console.log(count[input.length - 1]);
+  };
 
   part1();
+  part2();
 };
 await day10();
