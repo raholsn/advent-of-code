2728cb17597c67e53a886f75225645e2b02e9c61
Rasmus olsson <rasmus_olsson_@hotmail.com>
2020-12-17 21:04:14 +0100

    Day 11 p2

--- day11/index.ts
+++ day11/index.ts
@@ -1,16 +1,11 @@
 const day11 = async () => {
-  const input = (await Deno.readTextFile("./input.txt"))
-    .split("L")
-    .join("#")
-    .split("\n")
-    .map((x) => x.split(""));
-
   const isEmpty = (position: string) => position === "L";
   const isFloor = (position: string) => position === ".";
   const isAdjacentOccupied = (occupiedRange: string[]) =>
     occupiedRange.length >= 4 && occupiedRange.every((x) => x === "#");
+  const isAdjacentOccupiedP2 = (occupiedRange: number) => occupiedRange >= 5;
 
-  const isNoAdjacentOccupiedSeats = (occupiedRange: string[]) =>
+  const isNotAdjacentOccupied = (occupiedRange: string[]) =>
     !occupiedRange.some((x) => x === "#");
 
   const isEqual = (list1: string[][], list2: string[][]) => {
@@ -22,7 +17,13 @@ const day11 = async () => {
     return true;
   };
 
-  const part1 = () => {
+  const part1 = async () => {
+    const input = (await Deno.readTextFile("./input.txt"))
+      .split("L")
+      .join("#")
+      .split("\n")
+      .map((x) => x.split(""));
+
     let i = 0;
     while (i != -1) {
       const ic = JSON.parse(JSON.stringify(input));
@@ -43,7 +44,7 @@ const day11 = async () => {
           ] as string[];
 
           if (isEmpty(seat)) {
-            if (isNoAdjacentOccupiedSeats(adjacentSeats))
+            if (isNotAdjacentOccupied(adjacentSeats))
               input[index][index2] = "#";
           } else {
             if (isAdjacentOccupied(adjacentSeats.filter((x) => x === "#"))) {
@@ -66,7 +67,163 @@ const day11 = async () => {
     return hash;
   };
 
-  console.log(part1());
+  const calculateAdjacentDirections = (
+    direction:
+      | "left"
+      | "right"
+      | "top"
+      | "topLeft"
+      | "topRight"
+      | "below"
+      | "belowLeft"
+      | "belowRight",
+    index: number,
+    index2: number,
+    ic: string[][],
+    // deno-lint-ignore no-inferrable-types
+    xCount: number = 0
+  ) => {
+    switch (direction) {
+      case "left":
+        for (let i = index2; i >= 0; i--) {
+          if (ic[index][i - 1] === undefined) return "";
+          if (ic[index][i - 1] === "L" || ic[index][i - 1] === "#")
+            return ic[index][i - 1];
+        }
+
+        break;
+      case "right":
+        for (let i = index2; i < ic[index].length; i++) {
+          if (ic[index][i + 1] === undefined) return "";
+          if (ic[index][i + 1] === "L" || ic[index][i + 1] === "#")
+            return ic[index][i + 1];
+        }
+        break;
+      case "top":
+        for (let i = index; i >= 0; i--) {
+          if (ic[i - 1] === undefined) return "";
+          if (ic[i - 1][index2] === "L" || ic[i - 1][index2] === "#")
+            return ic[i - 1][index2];
+        }
+
+        break;
+      case "topLeft":
+        for (let i = index; i >= 0; i--) {
+          xCount++;
+          if (ic[i - 1] === undefined) return "";
+          if (ic[i - 1][index2 - xCount] === undefined) return "";
+          if (
+            ic[i - 1][index2 - xCount] === "L" ||
+            ic[i - 1][index2 - xCount] === "#"
+          )
+            return ic[i - 1][index2 - xCount];
+        }
+
+        break;
+      case "topRight":
+        for (let i = index; i >= 0; i--) {
+          xCount++;
+          if (ic[i - 1] === undefined) return "";
+          if (ic[i - 1][index2 + xCount] === undefined) return "";
+          if (
+            ic[i - 1][index2 + xCount] === "L" ||
+            ic[i - 1][index2 + xCount] === "#"
+          )
+            return ic[i - 1][index2 + xCount];
+        }
+        break;
+      case "below":
+        for (let i = index; i < ic[index].length; i++) {
+          if (ic[i + 1] === undefined) return "";
+          if (ic[i + 1][index2] === undefined) return "";
+          if (ic[i + 1][index2] === "L" || ic[i + 1][index2] === "#")
+            return ic[i + 1][index2];
+        }
+        break;
+      case "belowLeft":
+        for (let i = index; i < ic[index].length; i++) {
+          xCount++;
+          if (ic[i + 1] === undefined) return "";
+          if (ic[i + 1][index2 - xCount] === undefined) return "";
+          if (
+            ic[i + 1][index2 - xCount] === "L" ||
+            ic[i + 1][index2 - xCount] === "#"
+          )
+            return ic[i + 1][index2 - xCount];
+        }
+        break;
+      case "belowRight":
+        for (let i = index; i < ic[index].length; i++) {
+          xCount++;
+          if (ic[i + 1] === undefined) return "";
+          if (ic[i + 1][index2 + xCount] === undefined) return "";
+          if (
+            ic[i + 1][index2 + xCount] === "L" ||
+            ic[i + 1][index2 + xCount] === "#"
+          )
+            return ic[i + 1][index2 + xCount];
+        }
+        break;
+      default:
+        throw Error();
+    }
+  };
+
+  const part2 = async () => {
+    const input = (await Deno.readTextFile("./input.txt"))
+      .split("L")
+      .join("#")
+      .split("\n")
+      .map((x) => x.split(""));
+
+    let i = 1;
+    while (i != -1) {
+      const ic = JSON.parse(JSON.stringify(input));
+      for (let index = 0; index < input.length; index++) {
+        for (let index2 = 0; index2 < input[index].length; index2++) {
+          const seat = ic[index][index2];
+          if (isFloor(seat)) continue;
+
+          const adjacentSeats = [
+            calculateAdjacentDirections("left", index, index2, ic),
+            calculateAdjacentDirections("right", index, index2, ic),
+            calculateAdjacentDirections("top", index, index2, ic),
+            calculateAdjacentDirections("topLeft", index, index2, ic),
+            calculateAdjacentDirections("topRight", index, index2, ic),
+            calculateAdjacentDirections("below", index, index2, ic),
+            calculateAdjacentDirections("belowLeft", index, index2, ic),
+            calculateAdjacentDirections("belowRight", index, index2, ic),
+          ] as string[];
+
+          if (isEmpty(seat)) {
+            if (isNotAdjacentOccupied(adjacentSeats))
+              input[index][index2] = "#";
+          } else {
+            if (
+              isAdjacentOccupiedP2(
+                adjacentSeats.filter((x) => x === "#").length
+              )
+            ) {
+              input[index][index2] = "L";
+            }
+          }
+        }
+      }
+
+      if (isEqual(ic, input)) i = -1;
+    }
+
+    let hash = 0;
+    input.forEach((element) => {
+      element.forEach((element2) => {
+        if (element2 === "#") hash++;
+      });
+    });
+
+    return hash;
+  };
+  console.log(await part1());
+  console.log(await part2());
 };
 
 await day11();
