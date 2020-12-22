b4b7afd92c3f648a21c2b7b52cf2a227f0be5893
Rasmus olsson <rasmus_olsson_@hotmail.com>
2020-12-22 16:47:09 +0100

    Day 13 p2

--- day13/index.ts
+++ day13/index.ts
@@ -23,7 +23,26 @@ const day13 = async () => {
     return (waitingTime - myArrivalTime) * myBusId;
   };
 
+  const part2 = async () => {
+    const input = (await Deno.readTextFile("./input.txt")).split("\n");
+    const buses = input[1]
+      .replace(/x/g, "1")
+      .split(",")
+      .map((x) => +x);
+
+    let time = 0;
+    let multiplier = buses[0];
+    let i = 1;
+    while (i < buses.length) {
+      while ((time + i) % buses[i] !== 0) time += multiplier;
+      multiplier *= buses[i];
+      i++;
+    }
+    return time;
+  };
+
   console.log(await part1());
+  console.log(await part2());
 };
 
 await day13();
