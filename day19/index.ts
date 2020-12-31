7b937a5571b61ad1bdbdf53a0a9e3f7d0489fec7
Rasmus Olsson <rasmus_olsson_@hotmail.com>
2020-12-31 14:56:46 +0100

    Day 19 p2

--- day19/index.ts
+++ day19/index.ts
@@ -37,7 +37,6 @@ const day19 = async () => {
     ).split('\n\n');
     const rules = parseRules(unParsedRules);
     const messages = parseMessage(unParsedMessages);
-
     const rule0 = getRegex(rules[0], rules);
 
     const regexp = new RegExp(`^${rule0}$`);
@@ -49,7 +48,57 @@ const day19 = async () => {
     console.log(result);
   };
 
+  const getRegex2 = (rule: string, rules: Record<string, string>): string => {
+    let regex = '';
+    if (rule === rules[8]) {
+      regex = getRegex2(rules[42], rules);
+    } else if (rule === rules[11]) {
+      const regexList = [];
+      //try up to 5 repeats
+      for (let i = 1; i < 5; i++) {
+        regexList.push(
+          `(${getRegex2(rules[42], rules)}){${i}}(${getRegex2(
+            rules[31],
+            rules
+          )}){${i}}`
+        );
+      }
+      regex = `+(${regexList.join('()|')})`;
+    } else if (rule[0].match(/^[0-9]+$/) === null) {
+      regex = rule[1];
+    } else {
+      const [first, second] = rule.split(' | ');
+      if (second !== undefined)
+        regex = `(${getRegex2(first, rules)}|${getRegex2(second, rules)})`;
+      else {
+        regex = first
+          .split(' ')
+          .map((i) => getRegex2(rules[i.trim()], rules))
+          .join('');
+      }
+    }
+
+    return regex;
+  };
+
+  const part2 = async () => {
+    const [unParsedRules, unParsedMessages] = (
+      await Deno.readTextFile('./input.txt')
+    ).split('\n\n');
+    const rules = parseRules(unParsedRules);
+    const messages = parseMessage(unParsedMessages);
+
+    const rule0 = getRegex2(rules[0], rules);
+    const regexp = new RegExp(`^${rule0}$`);
+    const result = messages.reduce(
+      (prev, curr) => (regexp.test(curr) ? (+prev + 1).toString() : prev),
+      '0'
+    );
+
+    console.log(result);
+  };
   await part1();
+  await part2();
 };
 
 await day19();
